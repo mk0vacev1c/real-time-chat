905a8282b2067cb6d7aeacd2d9d205127e76d2c4
Milos <m_k0vacev1c@hotmail.com>
2022-06-29 23:25:29 +0200

    Add files via upload
    Backend

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/pusher/pusher-http-go"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Use(cors.New())
+
+	pusherClient := pusher.Client{
+		AppID:   "1430735",
+		Key:     "a01c0754bdfcc9315c90",
+		Secret:  "bf71316049bb0e47adc6",
+		Cluster: "eu",
+		Secure:  true,
+	}
+
+	app.Post("/api/messages", func(c *fiber.Ctx) error {
+		var data map[string]string
+
+		if err := c.BodyParser(&data); err != nil {
+			return err
+		}
+
+		pusherClient.Trigger("chat", "message", data)
+
+		return c.JSON([]string{})
+	})
+
+	app.Listen(":8000")
+}
